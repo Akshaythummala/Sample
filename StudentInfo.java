eaa63cdda7b09de9c7459791d60449e372e8ad9b
Akshaythummala <akshaythummalaa@gmail.com>
2026-03-28 19:58:45 +0530

    added new file

---
+++ StudentInfo.java
@@ -0,0 +1,19 @@
+public class StudentInfo {	
+    public static void main(String[] args) {	
+        String  name        = "Priya Reddy";
+        int     age         = 21;	
+        double  percentage  = 87.5;	
+        boolean isHosteler  = true;	 
+        char    section     = 'B';
+        long    phoneNumber = 9876543210L;	 
+        System.out.println("Name       : " + name);	
+        System.out.println("Age        : " + age);	 
+        System.out.println("Percentage : " + percentage + "%");	
+        System.out.println("Hosteler   : " + isHosteler);
+        System.out.println("Section    : " + section);	 
+        System.out.println("Phone      : " + phoneNumber);
+
+    }	 
+
+}
+ 	 
